3e07c8477e1fd4b0dc1bb326b1d6afe504a1b0b0
akanksha-codes23 <akanksha23r@gmail.com>
2026-03-24 18:53:31 +0530

    Fix CORS and SecurityConfig for deployed frontend

--- tasktrackerbackend/src/main/java/com/tasktracker/tasktracker/config/CorsConfig.java
+++ tasktrackerbackend/src/main/java/com/tasktracker/tasktracker/config/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+      config.setAllowedOrigins(List.of(
+    "http://localhost:5173", 
+    "https://fullstack-task-tracker-app-5.onrender.com" // deployed frontend URL
+));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
